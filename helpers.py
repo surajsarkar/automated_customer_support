cea52217ec2a55d116f555ceb20c312a279cf28d
Suraj Sarkar <suarjsarkar1002@gmail.com>
2023-12-27 20:29:23 +0530

    remove user id

--- helpers.py
+++ helpers.py
@@ -1,31 +1,31 @@
 from openai import OpenAI
 
 
-def get_query_response(client: OpenAI, message: str, user_id: str) -> str:
-    response = get_response_for_query(client, message, "user", user_id)
+def get_query_response(client: OpenAI, message: str,) -> str:
+    response = get_response_for_query(client, message, "user",)
     return response
 
 
-def query_convays_urgency(client: OpenAI, message: str, user_id: str) -> bool:
-    response = get_response_for_query(client, message, "user", user_id)
+def query_convays_urgency(client: OpenAI, message: str,) -> bool:
+    response = get_response_for_query(client, message, "user",)
     if "yes" in response.lower():
         return True
     return False
 
 
-def get_response_for_query(client: OpenAI, message: str, role: str, user_id: str) -> str:
+def get_response_for_query(client: OpenAI, message: str, role: str,) -> str:
     model = "gpt-3.5-turbo"
     message = {"role": role, "content": message}
-    msg_history = update_msg_history(user_id, message)
+    msg_history = update_msg_history(message)
     completion = client.chat.completions.create(model=model, messages=msg_history)
     return completion.choices[0].message.content
 
-def get_message_history(user_id: str) -> list:
+def get_message_history() -> list:
     import json
     import os
 
-    if not os.path.exists(f"message_history_{user_id}.json"):
-        with open(f"message_history_{user_id}.json", "w") as f:
+    if not os.path.exists(f"message_history.json"):
+        with open("message_history.json", "w") as f:
             json.dump([], f)
         return []
 
@@ -33,11 +33,11 @@ def get_message_history(user_id: str) -> list:
         msg_history = json.load(f)
     return msg_history
 
-def update_msg_history(user_id: str, message: dict):
+def update_msg_history(message: dict):
     import json
-    msg_history = get_message_history(user_id)
+    msg_history = get_message_history()
     msg_history.append(message)
-    with open(f"message_history_{user_id}.json", "w") as f:
+    with open("message_history.json", "w") as f:
         json.dump(msg_history, f)
     return msg_history
 
@@ -49,13 +49,13 @@ def notify_on_mail(message: str):
     sender_mail = os.environ.get("SENDER_MAIL")
     sender_pass = os.environ.get("SENDER_PASS")
     receivers_mail = os.environ.get("RECEIVERS_MAIL")
-    receivers_mail = [receiver_mail.strip() for receiver_mail in receivers_mail.split(",")]
 
     if not sender_mail or not sender_pass or not receivers_mail:
         print(
             "Please set the environment variables SENDER_MAIL, SENDER_PASS, RECEIVER_MAIL, for notification to work."
         )
         return
+    receivers_mail = [receiver_mail.strip() for receiver_mail in receivers_mail.split(",")]
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
     server.login(sender_mail, sender_pass)
--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-import pandas as pd 
+from flask import Flask
 
+app = Flask(__name__)
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    
